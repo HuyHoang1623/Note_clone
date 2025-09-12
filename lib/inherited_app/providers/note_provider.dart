c0c4985b40fd0defd103a1be67a6645546887bbb
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-09-12 13:54:42 +0700

    Update note_provider

--- lib/inherited_app/providers/note_provider.dart
+++ lib/inherited_app/providers/note_provider.dart
@@ -1,26 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:note_clone/core/models/note.dart';
 
-class NoteProvider extends ChangeNotifier {
+class NoteProvider extends InheritedWidget {
+  final NoteProviderState data;
+
+  const NoteProvider({super.key, required this.data, required Widget child})
+    : super(child: child);
+
+  static NoteProviderState of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<NoteProvider>()!.data;
+  }
+
+  @override
+  bool updateShouldNotify(NoteProvider oldWidget) => true;
+}
+
+class NoteProviderState extends State<StatefulWidget> {
   final List<Note> _notes = [];
 
-  List<Note> get notes => List.unmodifiable(_notes);
+  List<Note> get notes => _notes;
 
   void addNote(Note note) {
-    _notes.add(note);
-    notifyListeners();
+    setState(() {
+      _notes.add(note);
+    });
   }
 
-  void updateNote(Note updatedNote) {
-    final index = _notes.indexWhere((note) => note.id == updatedNote.id);
-    if (index != -1) {
-      _notes[index] = updatedNote;
-      notifyListeners();
-    }
+  void deleteNote(String id) {
+    setState(() {
+      _notes.removeWhere((n) => n.id == id);
+    });
   }
 
-  void deleteNoteById(String id) {
-    _notes.removeWhere((note) => note.id == id);
-    notifyListeners();
+  void editNote(Note updatedNote) {
+    setState(() {
+      final index = _notes.indexWhere((n) => n.id == updatedNote.id);
+      if (index != -1) {
+        _notes[index] = updatedNote;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return NoteProvider(data: this, child: Container());
   }
 }
