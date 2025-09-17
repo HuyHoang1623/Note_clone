c810a7a881001778a295c150d5f0ae9858a470f5
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-09-17 16:03:54 +0700

    Update main with BlocProvider cover

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:note_clone/original/pages/add_note_page.dart';
-import 'package:note_clone/original/pages/home_page.dart';
-import 'package:note_clone/core/signin_signup/signup_page.dart';
-import 'package:note_clone/original/pages/to_do_list_page.dart';
-import 'core/signin_signup/signin_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_clone/BLoC_app/BLoC/note/note_bloc.dart';
+import 'package:note_clone/BLoC_app/BLoC/task/task_bloc.dart';
+import 'package:note_clone/BLoC_app/pages/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NoteBloc()),
+        BlocProvider(create: (_) => TaskBloc()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  static _MyAppState? of(BuildContext context) =>
-      context.findAncestorStateOfType<_MyAppState>();
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  ThemeMode _themeMode = ThemeMode.dark;
-
-  void changeTheme(ThemeMode mode) {
-    setState(() {
-      _themeMode = mode;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,20 +25,6 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
 
       theme: ThemeData(
-        brightness: Brightness.light,
-        scaffoldBackgroundColor: Colors.white,
-        cardColor: const Color.fromARGB(255, 200, 200, 200),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-        ),
-        inputDecorationTheme: const InputDecorationTheme(
-          fillColor: Color(0xFFF0F0F0),
-        ),
-        textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.black)),
-      ),
-
-      darkTheme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF0B0B0B),
         cardColor: const Color.fromARGB(255, 64, 64, 64),
@@ -56,22 +33,16 @@ class _MyAppState extends State<MyApp> {
           foregroundColor: Colors.white,
         ),
         inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
           fillColor: Color(0xFF1E1E1E),
         ),
-        textTheme: const TextTheme(bodyLarge: TextStyle(color: Colors.white)),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
       ),
 
-      themeMode: _themeMode,
-
-      initialRoute: 'signin',
-
-      routes: {
-        'signin': (context) => const SignInPage(),
-        'signup': (context) => const SignUpPage(),
-        'homepage': (context) => const HomePage(),
-        'todolist': (context) => const ToDoListPage(),
-        'addnote': (context) => const AddNotePage(),
-      },
+      home: const HomePage(),
     );
   }
 }
