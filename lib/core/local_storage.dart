f9790a22295687ce11f92ad88fb2c522ce406bbf
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-09-26 23:26:41 +0700

    feat: add task local storage (save, delete, load)

--- lib/core/local_storage.dart
+++ lib/core/local_storage.dart
@@ -1,9 +1,11 @@
 import "dart:convert";
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:note_clone/core/models/note.dart';
+import 'package:note_clone/core/models/task.dart';
 
 class LocalStorage {
   static const String notePrefix = 'note_';
+  static const String taskPrefix = 'task_';
 
   static Future<void> saveNote(Note note) async {
     final prefs = await SharedPreferences.getInstance();
@@ -30,4 +32,29 @@ class LocalStorage {
   }
 
   static String noteKey(String id) => '$notePrefix$id';
+
+  static Future<void> saveTask(Task task) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(taskKey(task.id), jsonEncode(task.toJson()));
+  }
+
+  static Future<void> deleteTask(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove(taskKey(id));
+  }
+
+  static Future<List<Task>> loadTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    final keys = prefs.getKeys().where((k) => k.startsWith(taskPrefix));
+    List<Task> tasks = [];
+    for (var key in keys) {
+      final jsonStr = prefs.getString(key);
+      if (jsonStr != null) {
+        tasks.add(Task.fromJson(jsonDecode(jsonStr)));
+      }
+    }
+    return tasks;
+  }
+
+  static String taskKey(String id) => '$taskPrefix$id';
 }
