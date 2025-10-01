ea3c2a4467f75b1400803b9f95c52c2aa7113d11
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-10-02 04:29:44 +0700

    feat: add CloudStorage operations for Note

--- lib/BLoC_app/BLoC/note/note_bloc.dart
+++ lib/BLoC_app/BLoC/note/note_bloc.dart
@@ -2,32 +2,58 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_clone/BLoC_app/BLoC/note/note_event.dart';
 import 'package:note_clone/BLoC_app/BLoC/note/note_state.dart';
 import 'package:note_clone/core/local_storage.dart';
+import 'package:note_clone/core/cloud_storage.dart';
+import 'package:note_clone/core/models/note.dart';
 
 class NoteBloc extends Bloc<NoteEvent, NoteState> {
   NoteBloc() : super(const NoteState()) {
     on<LoadNotes>((event, emit) async {
-      final notes = await LocalStorage.loadNotes();
+      List<Note> notes = [];
+      try {
+        notes = await CloudStorage.getNotes();
+        for (var n in notes) {
+          await LocalStorage.saveNote(n);
+        }
+      } catch (e) {
+        final localNotes = await LocalStorage.loadNotes();
+        notes = localNotes
+            .map((e) => Note.fromJson(e as Map<String, dynamic>))
+            .toList();
+      }
       emit(state.copyWith(notes: notes, filteredNotes: notes));
     });
 
     on<AddNote>((event, emit) async {
       final updated = [...state.notes, event.note];
       emit(state.copyWith(notes: updated, filteredNotes: updated));
+
       await LocalStorage.saveNote(event.note);
+      try {
+        await CloudStorage.addNote(event.note);
+      } catch (_) {}
     });
 
+    /// UPDATE NOTE
     on<UpdateNote>((event, emit) async {
       final updated = state.notes
           .map((n) => n.id == event.note.id ? event.note : n)
           .toList();
       emit(state.copyWith(notes: updated, filteredNotes: updated));
+
       await LocalStorage.saveNote(event.note);
+      try {
+        await CloudStorage.addNote(event.note);
+      } catch (_) {}
     });
 
     on<DeleteNote>((event, emit) async {
       final updated = state.notes.where((n) => n.id != event.id).toList();
       emit(state.copyWith(notes: updated, filteredNotes: updated));
+
       await LocalStorage.deleteNote(event.id);
+      try {
+        await CloudStorage.deleteNote(event.id);
+      } catch (_) {}
     });
 
     on<SearchNotes>((event, emit) {
