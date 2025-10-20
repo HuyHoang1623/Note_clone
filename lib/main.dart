536f0a33b1d98fedfa7fc247a9dd340e1e79fc52
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-10-21 01:42:32 +0700

    feat: add firestore for main

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note_clone/BLoC_app/BLoC/note/note_bloc.dart';
 import 'package:note_clone/BLoC_app/BLoC/task/task_bloc.dart';
-import 'package:note_clone/BLoC_app/pages/home_page.dart';
 import 'package:note_clone/BLoC_app/BLoC/note/note_event.dart';
 import 'package:note_clone/BLoC_app/BLoC/task/task_event.dart';
-import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+import 'package:note_clone/core/signin_signup/signin_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  final user = FirebaseAuth.instance.currentUser;
+
   runApp(
     MultiBlocProvider(
       providers: [
-        BlocProvider(create: (_) => NoteBloc()..add(LoadNotes())),
+        BlocProvider(
+          create: (_) => NoteBloc()..add(LoadNotes(user?.uid ?? '')),
+        ),
         BlocProvider(create: (_) => TaskBloc()..add(LoadTasks())),
       ],
       child: const MyApp(),
@@ -30,7 +35,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
         brightness: Brightness.dark,
         scaffoldBackgroundColor: const Color(0xFF0B0B0B),
@@ -48,8 +52,7 @@ class MyApp extends StatelessWidget {
           bodyMedium: TextStyle(color: Colors.white),
         ),
       ),
-
-      home: const HomePage(),
+      home: const SignInPage(),
     );
   }
 }
