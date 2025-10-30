9942aaa3532869225bb9797367d4710a940416ec
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-10-31 01:48:39 +0700

    refactor: restructure TaskState into multiple subclasses

--- lib/BLoC_app/BLoC/task/task_state.dart
+++ lib/BLoC_app/BLoC/task/task_state.dart
@@ -1,11 +1,36 @@
 import 'package:note_clone/core/models/task.dart';
 
-class TaskState {
-  final List<Task> tasks;
+abstract class TaskState {}
 
-  const TaskState({this.tasks = const []});
+class TaskInitial extends TaskState {}
 
-  TaskState copyWith({List<Task>? tasks}) {
-    return TaskState(tasks: tasks ?? this.tasks);
+class TaskLoading extends TaskState {}
+
+class TaskLoaded extends TaskState {
+  final List<Task> personalTasks;
+  final List<Task> workspaceTasks;
+  final String? workspaceId;
+
+  TaskLoaded({
+    required this.personalTasks,
+    this.workspaceTasks = const [],
+    this.workspaceId,
+  });
+
+  TaskLoaded copyWith({
+    List<Task>? personalTasks,
+    List<Task>? workspaceTasks,
+    String? workspaceId,
+  }) {
+    return TaskLoaded(
+      personalTasks: personalTasks ?? this.personalTasks,
+      workspaceTasks: workspaceTasks ?? this.workspaceTasks,
+      workspaceId: workspaceId ?? this.workspaceId,
+    );
   }
+}
+
+class TaskError extends TaskState {
+  final String message;
+  TaskError(this.message);
 }
