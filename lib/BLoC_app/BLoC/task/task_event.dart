033da4e315f7ba38f9d620881b745ea7ec8e1a6f
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-10-31 01:50:04 +0700

    feat: add event(load, add, delete, toggle) for workspace task

--- lib/BLoC_app/BLoC/task/task_event.dart
+++ lib/BLoC_app/BLoC/task/task_event.dart
@@ -24,3 +24,26 @@ class ToggleTask extends TaskEvent {
   final String uid;
   ToggleTask(this.task, this.uid);
 }
+
+class LoadWorkspaceTasks extends TaskEvent {
+  final String workspaceId;
+  LoadWorkspaceTasks(this.workspaceId);
+}
+
+class AddWorkspaceTask extends TaskEvent {
+  final String workspaceId;
+  final Task task;
+  AddWorkspaceTask(this.workspaceId, this.task);
+}
+
+class DeleteWorkspaceTask extends TaskEvent {
+  final String workspaceId;
+  final String taskId;
+  DeleteWorkspaceTask(this.workspaceId, this.taskId);
+}
+
+class ToggleWorkspaceTask extends TaskEvent {
+  final String workspaceId;
+  final Task task;
+  ToggleWorkspaceTask(this.workspaceId, this.task);
+}
