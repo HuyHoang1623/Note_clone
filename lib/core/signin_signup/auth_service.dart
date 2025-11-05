da74b8e13d28e9f1aba083e25c0d407846c5878f
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-11-05 11:19:39 +0700

    feat: add(AuthService) sign in with email

--- lib/core/signin_signup/auth_service.dart
+++ lib/core/signin_signup/auth_service.dart
@@ -25,3 +25,22 @@
       throw Exception("Lỗi đăng ký: $e");
     }
   }
+
+  Future<void> signInWithEmail(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+      final user = _auth.currentUser!;
+      final userModel = UserModel(
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName ?? '',
+        photoUrl: user.photoURL,
+        provider: 'email',
+      );
+
+      await _saveUserToFirestore(userModel);
+    } catch (e) {
+      throw Exception("Lỗi đăng nhập: $e");
+    }
+  }
