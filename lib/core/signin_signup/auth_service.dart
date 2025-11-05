cd91eebe1526091ee467d916892593befef48ea6
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-11-05 11:20:04 +0700

    feat: add(AuthService) sign in with Google

--- lib/core/signin_signup/auth_service.dart
+++ lib/core/signin_signup/auth_service.dart
@@ -44,3 +44,31 @@
       throw Exception("Lỗi đăng nhập: $e");
     }
   }
+  Future<void> signInWithGoogle() async {
+    try {
+      await GoogleSignIn().signOut();
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) return;
+
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final userCredential = await _auth.signInWithCredential(credential);
+      final user = userCredential.user!;
+
+      final userModel = UserModel(
+        uid: user.uid,
+        email: user.email,
+        name: user.displayName,
+        photoUrl: user.photoURL,
+        provider: 'google',
+      );
+
+      await _saveUserToFirestore(userModel);
+    } catch (e) {
+      throw Exception("Lỗi đăng nhập Google: $e");
+    }
+  }
