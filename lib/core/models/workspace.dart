bda2d9706189e2db1409e4f5222d3965afbdfe17
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-11-14 09:05:13 +0700

    feat: add Firestore support for Workspace model

--- lib/core/models/workspace.dart
+++ lib/core/models/workspace.dart
@@ -15,6 +15,7 @@ class Workspace {
     required this.members,
     required this.createdAt,
   }) : id = id ?? DateTime.now().microsecondsSinceEpoch.toString();
+
   factory Workspace.fromJson(Map<String, dynamic> json) {
     return Workspace(
       id: json['id'],
@@ -36,3 +37,26 @@ class Workspace {
       'createdAt': createdAt.toIso8601String(),
     };
   }
+
+  factory Workspace.fromFirestore(Map<String, dynamic> json) {
+    return Workspace(
+      id: json['id'],
+      name: json['name'],
+      ownerId: json['ownerId'],
+      members: (json['members'] as List)
+          .map((m) => UserModel.fromJson(Map<String, dynamic>.from(m)))
+          .toList(),
+      createdAt: (json['createdAt'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'id': id,
+      'name': name,
+      'ownerId': ownerId,
+      'members': members.map((m) => m.toMap()).toList(),
+      'createdAt': Timestamp.fromDate(createdAt),
+    };
+  }
+}
