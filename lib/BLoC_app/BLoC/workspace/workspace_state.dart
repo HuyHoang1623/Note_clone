a3f1d72f09510ddfb8043d2eeafbe09f5c4a6a3e
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-11-20 01:20:11 +0700

    feat: Setup WorkspaceState base and core loading states

---
+++ lib/BLoC_app/BLoC/workspace/workspace_state.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+class WorkspaceState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class WorkspaceInitial extends WorkspaceState {}
+
+class WorkspaceLoading extends WorkspaceState {}
+
+class WorkspaceLoaded extends WorkspaceState {
+  final List<Map<String, dynamic>> workspaces;
+  final String? selectedId;
+  final String? selectedName;
+
+  WorkspaceLoaded({
+    required this.workspaces,
+    this.selectedId,
+    this.selectedName,
+  });
+
+  @override
+  List<Object?> get props => [workspaces, selectedId, selectedName];
+}
