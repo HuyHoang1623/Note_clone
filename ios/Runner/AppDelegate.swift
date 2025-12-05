bb7d143d148dadcf7db3dcad3a26bbf410105276
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-12-06 00:06:25 +0700

    feat: Implement MethodChannel handler for ios battery level

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,30 @@
-import Flutter
 import UIKit
+import Flutter
 
-@main
-@objc class AppDelegate: FlutterAppDelegate {
+@UIApplicationMain
+class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
+
+    let controller = window?.rootViewController as! FlutterViewController
+    let batteryChannel = FlutterMethodChannel(
+        name: "battery_channel",
+        binaryMessenger: controller.binaryMessenger
+    )
+
+    batteryChannel.setMethodCallHandler { (call, result) in
+      if call.method == "getIosBattery" {
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        let batteryLevel = Int(UIDevice.current.batteryLevel * 100)
+        result(batteryLevel)
+      } else {
+        result(FlutterMethodNotImplemented)
+      }
+    }
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
