31f991a0c1f15d1ebcae87ab03b50a0663ec9949
HuyHoang1623 <huyhoang16022003@gmail.com>
2025-12-17 13:44:21 +0700

    feat: add chat room id generator

--- lib/BLoC_app/pages/chat/chat_service.dart
+++ lib/BLoC_app/pages/chat/chat_service.dart
@@ -5,3 +5,9 @@ import 'package:note_clone/core/models/user.dart';
 class ChatService {
   final _firestore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
+
+  String getChatRoomId(String userId1, String userId2) {
+    List<String> ids = [userId1, userId2];
+    ids.sort();
+    return ids.join('_');
+  }
